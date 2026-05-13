d923747880c5e43046415f5723e866cbcccfe1ff
shlokzz <shlok.pd.joshi@gmail.com>
2026-05-13 09:08:56 +0545

    Implement duplicate unique key detection using Window Function and CTE

--- sql/02_data_cleaning.sql
+++ sql/02_data_cleaning.sql
@@ -15,3 +15,27 @@ FROM customer_churn;
 
 SELECT * 
 FROM customer_churn_staging;
+
+-- Identify the duplicated rows
+SELECT *, 
+ROW_NUMBER() OVER( 
+PARTITION BY customerID) AS row_num
+FROM customer_churn_staging;
+
+-- USE CTE to detect duplicate unique rows
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER( 
+PARTITION BY customerID) AS row_num
+FROM customer_churn_staging
+)
+
+SELECT * 
+FROM  duplicate_cte
+WHERE row_num > 1;
+
+-- Verify that customerID is a unique primary key to identify duplicates
+SELECT COUNT(customerID) AS total_rows,
+COUNT(DISTINCT customerID) AS unique_rows
+FROM customer_churn_staging;
