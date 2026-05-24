f0f33caa32ed9252029449722bd1c3342e86795b
shlokzz <shlok.pd.joshi@gmail.com>
2026-05-24 16:24:43 +0545

    Import necessary dependencies and load the data in dashboard_app file

---
+++ apps/page_components/dashboard_app.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+df = pd.read_csv("../dataset/clean dataset/final_telco_customer_churn_cleaned_dataset.csv")
+
