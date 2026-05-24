0bb192980ccbff93e79a9605d39342f076edad53
shlokzz <shlok.pd.joshi@gmail.com>
2026-05-24 10:10:59 +0545

    Fix index mismatch errors

--- apps/page_components/prediction_app.py
+++ apps/page_components/prediction_app.py
@@ -33,6 +33,7 @@ cats_col = [
     "online_security",
     "online_backup",
     "device_protection",
+    "tech_support",
     "streaming_tv",
     "streaming_movies",
     "contract",
@@ -46,11 +47,10 @@ def customer_churn_prediction(input_data):
     # convert inputs as a pandas DataFrame
     input_data_as_dataframe = pd.DataFrame([input_data])
 
-    # use the saved LabelEncoder
-    label_encoder_data = loaded_label_encoded_model.transform(input[cats_col])
-
     # use the saved OneHotEncoder model
-    one_hot_encoder_data = loaded_one_hot_encoder_model.transform(input[cats_col])
+    one_hot_encoder_data = loaded_one_hot_encoder_model.transform(
+        input_data_as_dataframe[cats_col]
+    )
     one_hot_encoder_df = pd.DataFrame(
         one_hot_encoder_data,
         columns=loaded_one_hot_encoder_model.get_feature_names_out(cats_col),
@@ -70,47 +70,52 @@ def customer_churn_prediction(input_data):
     final_input = final_input.reindex(columns=loaded_model_columns, fill_value=0)
 
     prediction = loaded_final_pipeline.predict(final_input)
-    return f"{prediction}"
+    return prediction
 
 
 def main():
 
     gender = st.selectbox("Select Gender", ["Male", "Female"])
-    senior_citizen = st.selectbox("Are You Above 60 Years Old?", ["Yes", "No"])
+    senior_citizen = st.number_input("Are You Above 60 Years Old (0 = No | 1=Yes)?")
     partner = st.selectbox("Are you married?", ["Yes", "No"])
     dependents = st.selectbox("Do you have dependents?", ["Yes", "No"])
     tenure = st.number_input(
         "How long have you have the tenure?", min_value=0, max_value=72
     )
+    phone_service = st.selectbox("Do you have a phone serive?", ["Yes", "No"])
+    
     multiple_lines = st.selectbox(
         "Do you have multiple lines?", ["Yes", "No", "No phone service"]
     )
     internet_service = st.selectbox(
         "Which internet service do you have?",
-        ["DSL", "Fiber Optic", "No phone service"],
+        ["DSL", "Fiber Optic", "No"],
     )
-    online_security = st.selectbox("Do you have online security?", ["Yes", "No"])
-    online_backup = st.selectbox("Do you have online backup?", ["Yes", "No"])
-    device_protection = st.selectbox("Do you have device protection?", ["Yes", "No"])
+    online_security = st.selectbox("Do you have online security?", ["Yes", "No","No internet service"])
+    online_backup = st.selectbox("Do you have online backup?", ["Yes", "No","No internet service"])
+    device_protection = st.selectbox("Do you have device protection?", ["Yes", "No","No internet service"])
+    tech_support = st.selectbox("Do you have tech support?", ["Yes", "No","No internet service"])
+    streaming_tv = st.selectbox("Do you stream in TV?", ["Yes", "No","No internet service"])
+    streaming_movies = st.selectbox("Do you stream movies?", ["Yes", "No","No internet service"])
     contract = st.selectbox(
         "Select Your Contract Type", ["Month-to-month", "One year", "Two year"]
     )
-    paper_less_billing = ("Do prefer paper less billing?", ["Yes", "No"])
-    payment_method = st.selectbox("Select your payment method?", ["Yes", "No"])
+    paper_less_billing = st.selectbox("Do you prefer paper less billing?", ["Yes", "No"])
+    payment_method = st.selectbox("Select your payment method?", ["Bank transfer (automatic)", "Credit card (automatic)", "Electronic check", "Mailed check"])
     monthly_charges = st.number_input(
         "Enter Your monthly charges?",
         min_value=0.0,
         max_value=200.0,
-        value=50.0,
+        value=0.0,
         step=1.0,
     )
     total_charges = st.number_input(
-        "Enter Your total charges?", min_value=0.0, max_value=10000, step=10.0
+        "Enter Your total charges?", min_value=0.0, max_value=10000.0, value=0.0, step=10.0
     )
     total_services = st.number_input(
-        "Enter how many servicews you have subscribed to?",
+        "Enter how many services you have subscribed to?",
         min_value=0,
-        max_value=10,
+        max_value=9,
         value=0,
         step=1,
     )
@@ -123,15 +128,19 @@ def main():
 
         inputs = {
             "gender": gender,
-            "senior_citzen": senior_citizen,
+            "senior_citizen": senior_citizen,
             "partner": partner,
             "dependents": dependents,
             "tenure": tenure,
+            "phone_service": phone_service,
             "multiple_lines": multiple_lines,
             "internet_service": internet_service,
             "online_security": online_security,
             "online_backup": online_backup,
             "device_protection": device_protection,
+            "tech_support": tech_support,
+            "streaming_tv": streaming_tv,
+            "streaming_movies": streaming_movies,
             "contract": contract,
             "paper_less_billing": paper_less_billing,
             "payment_method": payment_method,
@@ -139,6 +148,8 @@ def main():
             "total_charges": total_charges,
             "total_services": total_services,
         }
+
+        prediction = customer_churn_prediction(inputs)
 
         if prediction == 0:
             st.metric(label="Churn Prediction", value=False)
@@ -148,4 +159,6 @@ def main():
             st.metric(label="Churn Prediction", value=True)
             st.error("The customer is predicted To Be CHURNED.")
 
-main()
+
+if __name__ == "__main__":
+    main()
